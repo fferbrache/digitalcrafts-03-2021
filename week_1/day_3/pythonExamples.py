ed67a77dfd0b34581cdf8fe083bbbf9affca8491
frankie ferbrache <francisco.ferbrache@gmail.com>
2021-03-10 11:25:04 -0500

    first program

---
+++ week_1/day_3/pythonExamples.py
@@ -0,0 +1,26 @@
+# Variable
+# nameOfVariable = assignedValueToVariable
+# String === "", "word", "1", "functionName"
+greeting = "Hello World"
+# Integer === whole number, 1, 23, 934234, 9392
+classSize = 23
+# Float === 4.5, 4.55656565, 4.71234654736567658763856543678888886834567890
+floatExample = 2.323
+# Boolean === true or false, true, false, returns a TRUTHY or FALSY value
+variableName = True
+# Name, assignment operator (=), value that I am assigning
+sumOfNumbers = 1 + 1
+# List [], in JS they are called arrays, sequence of values followed by [value1,value2, etc]
+# [1, 2, 3]
+# ["strings", "string2", "blah blah blah"]
+# [False, True, True]
+# ["string", 1, 3.4, True, "string"]
+# nesting -- List inside of a List
+# ["list", ["list"]]
+# Dictionary {}
+# Sequence of labeled values {key: value}
+# {"stringName": "StringValue"}
+# {"FavoriteColor": "Red"}
+# print(sumOfNumbers == 2)
+# print("string" == "string")
+print("Hello World")
