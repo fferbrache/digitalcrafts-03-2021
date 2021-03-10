56f42aac11e0c527376f578f1df4c5c95eeb6560
frankie ferbrache <francisco.ferbrache@gmail.com>
2021-03-10 12:52:08 -0500

    string interpolation

---
+++ week_1/day_3/inputPython.py
@@ -0,0 +1,3 @@
+first_name_of_user = input("What is your first name?")
+lastNameOfUser = input("What is your last name?")
+print("Hello %s %s, welcome to python." % (first_name_of_user, lastNameOfUser))
