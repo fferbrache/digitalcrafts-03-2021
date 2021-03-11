2ee4ab56234806eb1ebe275197a560f6cdecc81e
frankie ferbrache <francisco.ferbrache@gmail.com>
2021-03-11 12:34:57 -0500

    lists and dictionaries

---
+++ week_1/day_4/listTuplesDictionaries.py
@@ -0,0 +1,27 @@
+# Lists
+# Contains stuff inside square brackets
+aList = ["list item one", 2, 3, "four"]
+
+# Dictionary
+# Sequence of LABELED values
+dictionary = {
+    "key": "value",
+    1: 1,
+    "mixture": 1,
+    "word": True}
+
+# "word"
+dictionaryOfUsers = {
+    "name": "Joe",
+    "age": 'blank',
+    "height": '6-1'
+}
+
+user = {
+    "name": "Joe",
+    "placesLived": {
+        "US": {"city": "tallanasty"},
+        "Overseas": {"Asia": "Philippines"},
+    },
+    "favoriteFoods": ["spaghetti", "chicken", "sushi", "lumpia"]
+}
