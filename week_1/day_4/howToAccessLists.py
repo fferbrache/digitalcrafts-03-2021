17f32856be6c4665bf141e0e91dc3288d475f212
frankie ferbrache <francisco.ferbrache@gmail.com>
2021-03-11 12:55:54 -0500

    accessing lists notes

---
+++ week_1/day_4/howToAccessLists.py
@@ -0,0 +1,18 @@
+pets = ["doggo", "cat", "iguana"]
+# Index in a list
+
+dog = pets[0]
+# cat = pets[1]
+# iguana = pets[2]
+print(dog)
+
+# demonSpawn = pets[-1]
+# -1 prints the value at the end of the list
+# print(demonSpawn)
+
+# If you want to access a range of values in a list, you use a number and a colon : and then end of the range you want to access
+# myFavoriteTwoAnimals = pets[0:2]
+# print(myFavoriteTwoAnimals)
+
+# demonSpawns = pets[-3:]
+# print(demonSpawns)
