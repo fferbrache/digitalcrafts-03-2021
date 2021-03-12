39a99d7365cd434662edc1a2f3a5a2edc3404d78
frankie ferbrache <francisco.ferbrache@gmail.com>
2021-03-12 15:32:04 -0500

    tech interview

---
+++ week_1/day_5/interview.py
@@ -0,0 +1,7 @@
+# Given an array A of straings made only from lowercase letters, return a list of all characters that show up in all strings within the list (including duplicates).
+
+# result = []
+# A = ["bella", "label", "roller"]
+
+# for item in A:
+#     print(A[item])
