150131f0a1d90e094a5d397259aa4b58371e037b
frankie ferbrache <francisco.ferbrache@gmail.com>
2021-03-29 11:18:03 -0400

    bootstrap examples

---
+++ week_3/day_5/chessBoard.py
@@ -0,0 +1,28 @@
+# Algorithm Challenge
+# if square is white, print blank space
+# if square is colored, print a hash
+# if row is greater than 8, print a new line /n
+
+# # # #
+# # # #
+# # # #
+# # # #
+
+def chessBoard():
+    return print("""
+     # # # #
+    # # # #
+     # # # #
+    # # # #
+     # # # #
+    # # # #
+     # # # #
+    # # # #
+    """)
+
+
+chessBoard()
+
+
+# we could use indexes, if 0 or even print o, odd print #
+# once string is greater
