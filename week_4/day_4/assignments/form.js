87137a474b58b991472835498ba2bc7fd25c457d
frankie ferbrache <francisco.ferbrache@gmail.com>
2021-04-01 15:07:22 -0400

    added flexbox to tip calculator

---
+++ week_4/day_4/assignments/form.js
@@ -0,0 +1,17 @@
+// const form = document.querySelector("form");
+
+// const validateName = (name) => {
+  // console.log(`Your name is ${name}`);
+//   myRegex = /[A-Za-z]+/;
+//   return myRegex.test(name);
+// };
+
+// form.addEventListener("submit", (event) => {
+//   const textInput = document.querySelector("#textInput");
+//   if(!validateName(textInput.value) === false){
+//     event.preventDefault();
+//     alert("Please enter in a valid name that doesn't contain numbers");
+//   }
+// });
+
+// Convert your tip calc to now be a form that runs a check of the input fields to make sure they are valid numbers.  Use JS for this, and add the eventlistener using JS as well.  Use Regex to find the correct pattern to compare your text to
