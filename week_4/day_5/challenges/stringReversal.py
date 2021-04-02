20cf3734793f14313f77174be4e0f5d9bd025adf
frankie ferbrache <francisco.ferbrache@gmail.com>
2021-04-02 11:51:18 -0400

    string reversal in python

---
+++ week_4/day_5/challenges/stringReversal.py
@@ -0,0 +1,9 @@
+# Write a funciton that reverses a given string
+# Do not use .reverse()
+
+def stringReversal(str):
+    reversedString = str[::-1]
+    print(reversedString)
+
+
+stringReversal("apple")
