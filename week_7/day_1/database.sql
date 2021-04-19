96bb27838d54ae6ad778b1010bd07c501a4cc4e2
frankie ferbrache <francisco.ferbrache@gmail.com>
2021-04-19 14:17:50 -0400

    first node server example

---
+++ week_7/day_1/database.sql
@@ -0,0 +1,19 @@
+CREATE DATABASE tododatabase;
+
+CREATE TABLE todo ( 
+  todo_id SERIAL PRIMARY KEY, -- MAKE THIS ID UNIQUE FROM ANY OTHER IDS
+  description VARCHAR(255)
+)
+
+-- CREATE TABLE gamer(
+--   gamer_id SERIAL PRIMARY KEY,
+--   userid SERIAL,
+--   userName VARCHAR(20)
+--   platform
+-- )
+
+-- CREATE TABLE platform (
+--   platform_id SERIAL PRIMARY KEY,
+--   platformName VARCHAR(20),
+--   gamerPlatform
+-- )
---
+++ week_7/day_1/index.js
@@ -0,0 +1,65 @@
+const http = require("http");
+const express = require("express");
+const app = express();
+const address = "127.0.0.1";
+const PORT = 3002;
+const { readFile } = require("fs");
+
+
+app.use(express.json());
+
+// const fs.readFile = require("fs")
+
+// destructuring
+// const user = {
+//   name: "joe",
+//   age: 31,
+// }
+
+// const {age} = user;
+// const age = user.age;
+
+// const server = http.createServer((req, res) => {
+//   console.log(req.url);
+
+//   if (req.url === "/hello-something") {
+//     res.statusCode = 200;
+//     res.end(`${req.url} is where you are at`)
+//   } else {
+//     res.statusCode = 200;
+//     res.setHeader("Content-Type", "text/plain");
+//     res.end("hello");
+//   }
+// });
+app.get("/", (req, res) => {
+  readFile("./index.html", "utf8", (err, html)=> {
+    res.send(html);
+  });
+  // res.end("Hello");
+});
+app.get("/home", (req, res) => {
+  // res.end("Hello another place")
+  readFile("homepage.html", "utf8", (err, html)=> {
+    res.send(html);
+  });
+});
+app.get("/userName", (req, res) => {
+  const userName = { user: "joe"};
+  
+  if (req.body !== "doge") {
+    res.statusCode = 400;
+    res.send("Uh oh, you have an error");
+  };
+  res.send(userName);
+});
+
+app.listen(PORT, () => {
+  console.log(`Your server is listening on ${PORT}`);
+});
+
+// server.listen(PORT, address, () => {
+//   console.log(`server is on ${address}:${PORT}`);
+// });
+
+
+
