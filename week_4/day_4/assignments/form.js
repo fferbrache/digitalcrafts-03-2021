cebd9f930eae2950daef810788693d417c227176
frankie ferbrache <francisco.ferbrache@gmail.com>
2021-04-20 10:40:56 -0400

    tip calculator form

--- week_4/day_4/assignments/form.js
+++ week_4/day_4/assignments/form.js
@@ -1,17 +1,33 @@
-// const form = document.querySelector("form");
+function tipCalc() {
+  const billAmount = Number(document.querySelector("#bill").value)
+  console.log(typeof billAmount)
+  const tipAmount = Number(document.querySelector("#tip").value)
+  console.log(typeof tipAmount)
+  const totalAmount = tipAmount * .01 * billAmount + billAmount
+  console.log(typeof totalAmount)
+  const target = document.querySelector(".total")
+  target.innerHTML = totalAmount
+  const newListItem = document.createElement("li")
+  newListItem.innerText = totalAmount - billAmount
+  console.log(tipAmount)
+  const list = document.getElementById("list")
+  list.append(newListItem)
+}
 
-// const validateName = (name) => {
-  // console.log(`Your name is ${name}`);
-//   myRegex = /[A-Za-z]+/;
-//   return myRegex.test(name);
-// };
+const form = document.querySelector("form");
 
-// form.addEventListener("submit", (event) => {
-//   const textInput = document.querySelector("#textInput");
-//   if(!validateName(textInput.value) === false){
-//     event.preventDefault();
-//     alert("Please enter in a valid name that doesn't contain numbers");
-//   }
-// });
+const validateForm = (name) => {
+  console.log(`Your name is ${name}`);
+  myRegex = /[A-Za-z]+/;
+  return myRegex.test(name);
+};
+
+form.addEventListener("submit", (event) => {
+  const textInput = document.querySelector("#textInput");
+  if(!validateName(textInput.value) === false){
+    event.preventDefault();
+    alert("Please enter in a valid name that doesn't contain numbers");
+  }
+});
 
 // Convert your tip calc to now be a form that runs a check of the input fields to make sure they are valid numbers.  Use JS for this, and add the eventlistener using JS as well.  Use Regex to find the correct pattern to compare your text to
