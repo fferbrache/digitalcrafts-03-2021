6b50411221fc683aa7bb0182b63e6523b34ca2ce
frankie ferbrache <francisco.ferbrache@gmail.com>
2021-04-20 16:21:12 -0400

    Jquery Example

--- week_5/day_3/examples/script.js
+++ week_5/day_3/examples/script.js
@@ -11,7 +11,31 @@ const $jdivContainer = $("<div>");
 const $header3 = $("<h3>", {
   text: "Joe is Here",
 });
+// target csss on a jquery element
+$header3.css("background-color", "red");
 
 $jdivContainer.append($header3);
 console.log($jdivContainer);
 $(document.body).append($jdivContainer);
+
+// button.addEventListener('click', () => {})
+
+// $btn.on('click', () => {})
+
+// fetch("url", {})
+
+// PROMISE is a thenable Object
+// .then() is for RESOLUTION
+// .catch() is for REJECTION
+
+$ajax({
+  url: "https://pokeapi.co/api/v2/pokemon/ditto",
+  // header {
+  //   Accept: "application/json",
+  // },
+}).then((res) => {
+  console.log(res.name);
+})
+.catch((valueReturned) => {
+  console.log(valueReturned)
+});
