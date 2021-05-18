67fd06cfc718b0a8b375af47e263da26d169d73c
frankie ferbrache <francisco.ferbrache@gmail.com>
2021-05-18 12:07:59 -0400

    added more pokemon to data

--- week_11/day_2/examples/props-passing/src/data/data.js
+++ week_11/day_2/examples/props-passing/src/data/data.js
@@ -31,6 +31,256 @@ export const data = [
           back: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/back/4.png",
         },
       },
+      {
+        id: 5,
+        name: "charmeleon",
+        hp: 58,
+        sprites: {
+          front:
+            "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/5.png",
+          back: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/back/5.png",
+        },
+      },
+      {
+        id: 6,
+        name: "charizard",
+        hp: 78,
+        sprites: {
+          front:
+            "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/6.png",
+          back: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/back/6.png",
+        },
+      },
+      {
+        id: 7,
+        name: "squirtle",
+        hp: 44,
+        sprites: {
+          front:
+            "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/7.png",
+          back: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/back/7.png",
+        },
+      },
+      {
+        id: 8,
+        name: "wartortle",
+        hp: 59,
+        sprites: {
+          front:
+            "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/8.png",
+          back: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/back/8.png",
+        },
+      },
+      {
+        id: 9,
+        name: "blastoise",
+        hp: 79,
+        sprites: {
+          front:
+            "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/9.png",
+          back: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/back/9.png",
+        },
+      },
+      {
+        id: 10,
+        name: "caterpie",
+        hp: 45,
+        sprites: {
+          front:
+            "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/10.png",
+          back: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/back/10.png",
+        },
+      },
+      {
+        id: 11,
+        name: "metapod",
+        hp: 50,
+        sprites: {
+          front:
+            "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/11.png",
+          back: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/back/11.png",
+        },
+      },
+      {
+        id: 12,
+        name: "butterfree",
+        hp: 60,
+        sprites: {
+          front:
+            "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/12.png",
+          back: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/back/12.png",
+        },
+      },
+      {
+        id: 13,
+        name: "weedle",
+        hp: 40,
+        sprites: {
+          front:
+            "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/13.png",
+          back: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/back/13.png",
+        },
+      },
+      {
+        id: 14,
+        name: "kakuna",
+        hp: 45,
+        sprites: {
+          front:
+            "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/14.png",
+          back: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/back/14.png",
+        },
+      },
+      {
+        id: 15,
+        name: "beedrill",
+        hp: 65,
+        sprites: {
+          front:
+            "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/15.png",
+          back: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/back/15.png",
+        },
+      },
+      {
+        id: 16,
+        name: "pidgey",
+        hp: 40,
+        sprites: {
+          front:
+            "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/16.png",
+          back: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/back/16.png",
+        },
+      },
+      {
+        id: 17,
+        name: "pidgeotto",
+        hp: 63,
+        sprites: {
+          front:
+            "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/17.png",
+          back: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/back/17.png",
+        },
+      },
+      {
+        id: 18,
+        name: "pidgeot",
+        hp: 83,
+        sprites: {
+          front:
+            "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/18.png",
+          back: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/back/18.png",
+        },
+      },
+      {
+        id: 19,
+        name: "rattata",
+        hp: 30,
+        sprites: {
+          front:
+            "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/19.png",
+          back: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/back/19.png",
+        },
+      },
+      {
+        id: 20,
+        name: "raticate",
+        hp: 55,
+        sprites: {
+          front:
+            "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/20.png",
+          back: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/back/20.png",
+        },
+      },
+      {
+        id: 21,
+        name: "spearow",
+        hp: 40,
+        sprites: {
+          front:
+            "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/21.png",
+          back: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/back/21.png",
+        },
+      },
+      {
+        id: 22,
+        name: "fearow",
+        hp: 65,
+        sprites: {
+          front:
+            "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/22.png",
+          back: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/back/22.png",
+        },
+      },
+      {
+        id: 23,
+        name: "ekans",
+        hp: 35,
+        sprites: {
+          front:
+            "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/23.png",
+          back: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/back/23.png",
+        },
+      },
+      {
+        id: 24,
+        name: "arbok",
+        hp: 60,
+        sprites: {
+          front:
+            "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/24.png",
+          back: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/back/24.png",
+        },
+      },
+      {
+        id: 25,
+        name: "pikachu",
+        hp: 35,
+        sprites: {
+          front:
+            "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/25.png",
+          back: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/back/25.png",
+        },
+      },
+      {
+        id: 26,
+        name: "raichu",
+        hp: 60,
+        sprites: {
+          front:
+            "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/26.png",
+          back: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/back/26.png",
+        },
+      },
+      {
+        id: 27,
+        name: "sandshrew",
+        hp: 50,
+        sprites: {
+          front:
+            "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/27.png",
+          back: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/back/27.png",
+        },
+      },
+      {
+        id: 28,
+        name: "sandslash",
+        hp: 75,
+        sprites: {
+          front:
+            "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/28.png",
+          back: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/back/28.png",
+        },
+      },
+      {
+        id: 29,
+        name: "nidoran-f",
+        hp: 55,
+        sprites: {
+          front:
+            "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/29.png",
+          back: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/back/29.png",
+        },
+      },
     ],
   },
 ];
