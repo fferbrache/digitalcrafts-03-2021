92abae76dae325bd1ad0252a57b40eed53955196
frankie ferbrache <francisco.ferbrache@gmail.com>
2021-05-19 10:34:34 -0400

    working form with styling

--- week_11/day_3/examples/app/src/App.js
+++ week_11/day_3/examples/app/src/App.js
@@ -3,7 +3,21 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <h1>Hello World</h1>
+      <h1>Forms React</h1>
+      <div className="shipping-form-container">
+      <form action="" className="shipping-form">
+        <input className="shipping-input-text" type="text" placeholder="First Name"/>
+        <input className="shipping-input-text" type="text" placeholder="Last Name"/>
+        <input className="shipping-input-text-street" type="text" placeholder="Street Address"/>
+        <input className="shipping-input-text-apt" type="text" placeholder="Apt, Suite"/>
+        <input className="shipping-input-text" type="text" placeholder="City"/>
+        <input className="shipping-input-text" type="text" placeholder="State"/>
+        <input className="shipping-input-text" type="text" placeholder="Postal Code"/>
+        <input className="shipping-input-text" type="text" placeholder="Country"/>
+        <input className="shipping-input-text" type="text" placeholder="Phone"/>
+        <input className="shipping-input-button" type="Submit" placeholder="Submit"/>
+      </form>
+      </div>
     </div>
   );
 }
