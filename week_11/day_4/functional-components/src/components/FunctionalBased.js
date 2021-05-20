d57dcdfbfee143687130efee08c4b0909ab98176
frankie ferbrache <francisco.ferbrache@gmail.com>
2021-05-20 11:14:13 -0400

    added status button

--- week_11/day_4/functional-components/src/components/FunctionalBased.js
+++ week_11/day_4/functional-components/src/components/FunctionalBased.js
@@ -2,11 +2,14 @@ import React, { useState } from 'react';
 
 const FunctionalBased = () => {
   const [loadingMessage, setLoadingMessage] = useState("loading");
+  const [status, setStatus] = useState("Code Red")
   return (
     <div>
     <h1>Functional Based Component</h1>
     {loadingMessage}
+    {status}
     <button onClick={() => setLoadingMessage("finished")}>Finished Loading</button>
+    <button onClick={() => setStatus("Mountain Dew")}>Status</button>
   </div>
   )
 
